42716dc352967a64fd66f89cd53e703e49eb50ba
nolimitmaxi <maximitov563@gmail.com>
2026-05-07 10:09:08 +0300

    feat: call DbSeeder on application startup

--- FinalProjectsManager/Program.cs
+++ FinalProjectsManager/Program.cs
@@ -9,6 +9,12 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var context = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    DbSeeder.Seed(context);
+}
+
 app.UseHttpsRedirection();
 app.UseAuthorization();
 app.MapControllers();
