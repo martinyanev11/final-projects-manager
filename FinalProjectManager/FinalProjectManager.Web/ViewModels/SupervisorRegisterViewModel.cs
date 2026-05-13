786ed960de95de90d18f5e19a98ecb496ae56547
Angel Delchev <hunterwolf123.529@gmail.com>
2026-05-13 11:16:47 +0300

    Translate Models and ViewModels to Bulgarian

--- FinalProjectManager/FinalProjectManager.Web/ViewModels/SupervisorRegisterViewModel.cs
+++ FinalProjectManager/FinalProjectManager.Web/ViewModels/SupervisorRegisterViewModel.cs
@@ -4,7 +4,7 @@ namespace FinalProjectManager.Web.ViewModels;
 
 public class SupervisorRegisterViewModel
 {
-    [Required, MaxLength(100), Display(Name = "Full Name")]
+    [Required, MaxLength(100), Display(Name = "Имена")]
     public string FullName { get; set; } = string.Empty;
 
     [Required, EmailAddress, MaxLength(100)]
@@ -13,6 +13,6 @@ public class SupervisorRegisterViewModel
     [Required, DataType(DataType.Password), MinLength(6)]
     public string Password { get; set; } = string.Empty;
 
-    [Required, DataType(DataType.Password), Compare(nameof(Password)), Display(Name = "Confirm Password")]
+    [Required, DataType(DataType.Password), Compare(nameof(Password)), Display(Name = "Потвърди парола")]
     public string ConfirmPassword { get; set; } = string.Empty;
 }
